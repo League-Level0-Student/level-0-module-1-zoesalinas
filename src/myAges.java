049eda00c2494bea8f096236c4b2e82a5164c37f
League Student <league@iMac-34.attlocal.net>
2019-08-19 19:47:24 -0700

    Finished module 1 level 0

---
+++ src/myAges.java
@@ -0,0 +1,8 @@
+
+public class myAges {
+public static void main(String[] args) {
+	for (int i = 2009; i < 2020; i++) {
+		System.out.println("In "+ i + " I was " +  (i - 2009) + " years old");
+	}
+}
+}
